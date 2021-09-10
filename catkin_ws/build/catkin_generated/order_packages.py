6a5d947f4f278d92e9b5be4cb84e8803cdefdffb
KtGunn <ktgunn@hotmail.com>
2021-09-10 12:35:34 -0400

    Adding catkin_ws

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/rugrat/ktg.d/udacity.d/rsnd.d/RSND_MapMyWorld/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
---
+++ catkin_ws/build/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /home/rugrat/ktg.d/udacity.d/rsnd.d/RSND_MapMyWorld/catkin_ws/build/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
